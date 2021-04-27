7ccd3311835931602ab48584ffdf44c4a13f7409
Ari Fisher <bb8andr2d2forlife@gmail.com>
2021-04-26 21:04:06 -0400

    default

---
+++ main.cpp
@@ -0,0 +1,152 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <emscripten.h>
+#include "spritesheet.h"
+/**
+ * Inverse square root of two, for normalising velocity
+ */
+#define REC_SQRT2 0.7071067811865475 
+
+/**
+ * Set of input states
+ */
+enum input_state
+{
+    NOTHING_PRESSED = 0,
+    UP_PRESSED = 1,
+    DOWN_PRESSED = 1<<1,
+    LEFT_PRESSED = 1<<2,
+    RIGHT_PRESSED = 1<<3
+};
+
+/**
+ * Context structure that will be passed to the loop handler
+ */
+struct context
+{
+    SDL_Renderer *renderer;
+
+    /**
+     * Rectangle that the owl texture will be rendered into
+     */
+    SDL_Surface* window;
+    SDL_Rect dest;
+    SDL_Texture *owl_tex;
+    Spritesheet hero;
+    int active_state;
+
+    /**
+     * x and y components of owl's velocity
+     */
+    int owl_vx;
+    int owl_vy;
+};
+
+
+
+/**
+ * Processes the input events and sets the velocity
+ * of the owl accordingly
+ */
+void process_input(struct context *ctx)
+{
+    SDL_Event event;
+
+    while (SDL_PollEvent(&event)) {
+        switch (event.key.keysym.sym)
+        {
+            case SDLK_UP:
+                if (event.key.type == SDL_KEYDOWN)
+                    ctx->active_state |= UP_PRESSED;
+                else if (event.key.type == SDL_KEYUP)
+                    ctx->active_state ^= UP_PRESSED;
+                break;
+            case SDLK_DOWN:
+                if (event.key.type == SDL_KEYDOWN)
+                    ctx->active_state |= DOWN_PRESSED;
+                else if (event.key.type == SDL_KEYUP)
+                    ctx->active_state ^= DOWN_PRESSED;
+                break;
+            case SDLK_LEFT:
+                if (event.key.type == SDL_KEYDOWN)
+                    ctx->active_state |= LEFT_PRESSED;
+                else if (event.key.type == SDL_KEYUP)
+                    ctx->active_state ^= LEFT_PRESSED;
+                break;
+            case SDLK_RIGHT:
+                if (event.key.type == SDL_KEYDOWN)
+                    ctx->active_state |= RIGHT_PRESSED;
+                else if (event.key.type == SDL_KEYUP)
+                    ctx->active_state ^= RIGHT_PRESSED;
+                break;
+            default:
+                break;
+        }
+    }
+
+    ctx->owl_vy = 0;
+    ctx->owl_vx = 0;
+    if (ctx->active_state & UP_PRESSED)
+        ctx->owl_vy = -5;
+    if (ctx->active_state & DOWN_PRESSED)
+        ctx->owl_vy = 5;
+    if (ctx->active_state & LEFT_PRESSED)
+        ctx->owl_vx = -5;
+    if (ctx->active_state & RIGHT_PRESSED)
+        ctx->owl_vx = 5;
+
+    if (ctx->owl_vx != 0 && ctx->owl_vy != 0)
+    {
+        ctx->owl_vx *= REC_SQRT2;
+        ctx->owl_vy *= REC_SQRT2;
+    }
+}
+
+/**
+ * Loop handler that gets called each animation frame,
+ * process the input, update the position of the owl and 
+ * then render the texture
+ */
+void loop_handler(void *arg)
+{
+   struct context *ctx = (context*)arg;
+
+    int vx = 0;
+    int vy = 0;
+    process_input(ctx);
+
+    ctx->dest.x += ctx->owl_vx;
+    ctx->dest.y += ctx->owl_vy;
+
+    SDL_RenderClear(ctx->renderer);
+    ctx->hero.select_sprite(0,0);
+    ctx->hero.draw_selected_sprite(ctx->window, &ctx->dest);
+    SDL_RenderPresent(ctx->renderer);
+}
+
+int main()
+{
+    SDL_Window *window;
+    struct context ctx;
+
+    SDL_Init(SDL_INIT_VIDEO);
+    SDL_CreateWindowAndRenderer(600, 400, 0, &window, &ctx.renderer);
+    SDL_SetRenderDrawColor(ctx.renderer, 255, 255, 255, 255);
+
+    // get_owl_texture(&ctx);
+    ctx.active_state = NOTHING_PRESSED;
+    ctx.dest.x = 200;
+    ctx.dest.y = 100;
+    ctx.owl_vx = 0;
+    ctx.owl_vy = 0;
+    ctx.window = SDL_GetWindowSurface(window);
+    ctx.hero = Spritesheet("assets/adventurer.png", 7, 11);
+
+    /**
+     * Schedule the main loop handler to get 
+     * called on each animation frame
+     */
+    emscripten_set_main_loop_arg(loop_handler, &ctx, -1, 1);
+
+    return 0;
+}
---
+++ spritesheet.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <SDL2/SDL.h>
+
+class Spritesheet
+{
+public:
+    Spritesheet(char const *path, int row, int column);
+    ~Spritesheet();
+    Spritesheet();
+    void select_sprite(int x, int y);
+    void draw_selected_sprite(SDL_Surface *window_surface, SDL_Rect *position);
+
+    SDL_Surface* get_selected_sprite();
+private:
+    SDL_Rect     m_clip;
+    SDL_Surface *m_spritesheet_image;
+};
